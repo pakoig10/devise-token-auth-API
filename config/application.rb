188c252880ed998394989ca8e8640ac22c88abbd
Paco Iglesias <jiglesias1@ucol.mx>
2020-12-09 12:33:48 -0600

    Finish api auth setup and configuration

--- config/application.rb
+++ config/application.rb
@@ -22,14 +22,15 @@ Bundler.require(*Rails.groups)
 module DeviseTAApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*' 
+        resource '*',
+        headers: :any,
+        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+        methods: [:get, :post, :options, :delete, :put, :patch]
+      end
+    end
     config.generators.system_tests = nil
   end
 end
